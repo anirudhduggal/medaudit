972aff30c88ae0b024c4c4edbc66561343f313d4
AdwaitJ <advaitjoshi27@gmail.com>
2019-03-28 17:15:31 -0400

    hl fuzzer
    hl7_fuzzer

--- src/hl7/hl7Scripts/hl7_fuzzer.py
+++ src/hl7/hl7Scripts/hl7_fuzzer.py
@@ -1,13 +1,59 @@
+import socket
+import logging
 import string,random
-import hl7_messageSender as messageSender
+
+consoleOuputFileName = "../networkFiles/hl7_fuzzer.log"
+
+def send(host, port, message, timeout):
+    # timeout for the reply, if the reply is not received in timeout parameter, close connection
+
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+    global consoleOuputFileName
+    logging.basicConfig(filename=consoleOuputFileName, level=logging.DEBUG, filemode='w')
+
+    try:
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # timeout = int(timeout)
+        conn.settimeout(int(timeout))
+        conn.connect((host, int(port)))
+
+        # start and end block - used to define start and End of message in MLLP
+        start_block = b'\x0b'
+        end_block = b'\x1c'
+        carriage_return = b'\x0d'
+
+        # create message here
+        msg = start_block.decode() + message + end_block.decode() + carriage_return.decode()
+
+        # send message
+        logging.debug("Sending Message: " + str(msg.encode()))
+        conn.send(msg.encode())
+
+        # recieve ack / nack message (buffer size 4096)
+        try:
+            ack = conn.recv(4096)
+            if ack:
+                writeAck = ack.decode()
+                logging.debug("Recieved a reply: " + writeAck)
+                return writeAck
+            else:
+                logging.debug("Recieved no reply ")
+        except socket.timeout as e:
+            logging.debug("Exception: " + str(e))
+            return e
+        conn.close()
+    # send an exception if connection fails
+    except socket.error as e:
+        logging.debug("Exception: " + str(e))
+        return e
 
 def start_fuzzing(message, ip_address, port, mode):
-    obj = messageSender.Hl7Message()
     print("Fuzzing started")
-    for i in range(0, 2048):
+    for i in range(0, 1024):
         new_message = new_locate_replace(message, mode, i)
         try:
-            response = obj.send(ip_address, port, new_message, 5, "log_file.txt")
+            response = send(ip_address, port, new_message, 5)
             print("Response received for request " + str(i) + " \n" + response)
         except Exception as e:
             print("Possible crash at message: ", new_message , "\n Error: " + str(e) )
@@ -46,6 +92,10 @@ def new_locate_replace(message,mode, count):
     #print("First string" + message)
     #print("Final string is " + final_str)
     return final_str
+
+
+
+
 
 
 
